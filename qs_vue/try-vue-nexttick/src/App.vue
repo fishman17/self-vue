57ab28b42f69aa4b420cf78ebd1c8eeed1d19b94
fishman17 <734583898@qq.com>
2018-01-12 14:28:01 +0800

    'nexttick'

---
+++ qs_vue/try-vue-nexttick/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+      <div>姓名：{{name}}</div>
+      <ul ref="movies">
+        <li v-for="item in movies" :key="item.id">{{item.title}}</li>
+      </ul> 
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+  },
+  data(){
+    return{
+      name: 'sss',
+      movies: [],
+    }
+  },
+  mounted() {
+    console.log('sssggg');
+    this.name = '分分分分分恩';
+    fetch('https://www.easy-mock.com/mock/5a09867f28b23066479b8365/ajaxData/movie#!method=get')
+    .then(data=>data.json())      //二进制流转换成json
+    .then(result =>{
+      this.movies = result.subjects;
+      
+      // console.log(this.$refs.movies.getElementsByTagName('li')[0].style.color = 'red');
+       this.$nextTick(()=>{
+     this.$refs.movies.getElementsByTagName('li')[0].style.color = 'red'
+      })
+      console.log(result);
+    })
+    .catch(err=>{
+      console.log(err);
+    });
+  },
+  beforeCreate(){
+    this.$nextTick(()=>{
+      this.name = 'bsfg';
+    })
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
